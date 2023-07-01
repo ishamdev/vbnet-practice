8fd584f393a5c172f22f04607ea74db62c077077
ishamdev <ishamdev2@gmail.com>
2023-07-01 19:58:07 +0700

    Add Latihan 12

---
+++ Latihan12/Latihan12/Form1.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        ListBox = New ListBox()
+        Label3 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        Label4 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(29, 83)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(130, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Besar tabungan / Bulan"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(29, 115)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(97, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Lama Menabung"
+        ' 
+        ' ListBox
+        ' 
+        ListBox.FormattingEnabled = True
+        ListBox.ItemHeight = 15
+        ListBox.Location = New Point(29, 190)
+        ListBox.Name = "ListBox"
+        ListBox.Size = New Size(281, 184)
+        ListBox.TabIndex = 2
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(292, 115)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(37, 15)
+        Label3.TabIndex = 3
+        Label3.Text = "Bulan"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(186, 80)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 4
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(186, 112)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 5
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(29, 156)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(257, 23)
+        Button1.TabIndex = 6
+        Button1.Text = "Simulasi Tabungan"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.Location = New Point(56, 13)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(243, 25)
+        Label4.TabIndex = 7
+        Label4.Text = "Aplikasi Simulasi Tabungan"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(341, 395)
+        Controls.Add(Label4)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label3)
+        Controls.Add(ListBox)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ListBox As ListBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Latihan12/Latihan12/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Dim tabungan, lama, x, saldo As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        tabungan = TextBox1.Text
+        lama = TextBox2.Text
+        saldo = tabungan
+        For x = 1 To lama
+            ListBox.Items.Add("saldo tabungan ke - " + CStr(x) + " = " + CStr(saldo))
+
+            saldo = saldo + tabungan
+
+        Next
+
+    End Sub
+
+
+End Class
